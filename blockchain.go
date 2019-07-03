136144fd1f830cb4cbb871422b841ad2f989db50
prospect-man <46277489+prospect-man@users.noreply.github.com>
2019-07-03 21:53:03 +0100

    Update blockchain.go

--- blockchain.go
+++ blockchain.go
@@ -1,16 +1,39 @@
-package blockchain
+package main
 
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type transaction struct {
+	Sender    string
+	Recipient string
+	Amount    int
+}
+
+  func calculateHash() {
+    return SHA256(index + Timestamp + Data + PrevHash + Nonce
+}
+		  
 type block struct {
 
-	Index         		// The index of the block
-	Timestamp 		    // Time stamp in epoch
-	Data      		    // The data we want to store
-	PrevHash   		    // chain/hash of previous block
-	Nonce     	    	// String that needs to be mined
-	CalclHash    			//calcul Hash     	    	
+	Index         	int	// The index of the block
+	Timestamp 	float64	    // Time stamp in epoch   
+	transactions    []transaction // The data we want to store
+	PrevHash   	string	    // chain/hash of previous block
+	Nonce     	int    	// String that needs to be mined
+	CalclHash    	calculateHash() //calcul Hash     	    	
 	CurrHash   			// Current hash
 
 }
-  func calculateHash() {
-    return SHA256(index + Timestamp + Data + PrevHash + Nonce
-  }
+		  
+type Blockchain struct {
+	current_transactions []transaction
+	chain                []block
+	nodes                []string
+}
